a9ccc7fc4281dbd96fdfd13d475e46424e2eb267
Javeriajamal <javeriajamal034@gmail.com>
2024-06-29 12:11:06 -0700

    Adding a simple CLI Calculator Project

---
+++ simple_Calculator/index.js
@@ -0,0 +1,31 @@
+//#!/usr/bin/env node
+import inquirer from "inquirer";
+//printing a welcome message to users
+console.log("\n\t Welcome to '\Javeria Jamal's\' -CLI Simple Calculator \n");
+// Asking questions from users through inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// performing task using the operators and numbers that user selects
+if (answers.operators === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operators === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operators === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operators === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input!");
+}
